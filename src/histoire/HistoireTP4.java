bd03ef7729e052f2011e9a849c8121e50b4f1c6c
Bahrichadha23 <chathabahri55@gmail.com>
2024-04-09 17:04:03 +0200

    PierrSabre

--- src/histoire/HistoireTP4.java
+++ src/histoire/HistoireTP4.java
@@ -11,6 +11,29 @@ public class HistoireTP4 {
 		prof.direBonjour();
 		prof.boire();
 		prof.acheter("jeu",2);
+		Commercant proff=new Commercant()
+		Commercant marco = new Commercant("Marco", 20);
+        Yakuza yakuLeNoir = new Yakuza("Yaku Le Noir", "whisky", 30, "Warsong");
+        marco.direBonjour();
+        yakuLeNoir.direBonjour();
+        yakuLeNoir.extorquer(marco);
+        marco.recevoir(15);
+        marco.boire();
+        Commercant marco = new Commercant("Marco", 20);
+        Yakuza yakuLeNoir = new Yakuza("Yaku Le Noir", "whisky", 30, "Warsong");
+        Ronin roro = new Ronin("Roro", 60);
+
+        roro.direBonjour();
+        roro.donner(marco);
+        Ronin roro = new Ronin("Roro", 60);
+        Yakuza yakuLeNoir = new Yakuza("Yaku Le Noir", "whisky", 45, "Warsong", 0); // Réputation initialement à 0
+
+        roro.direBonjour();
+        roro.provoquer(yakuLeNoir);
+        yakuLeNoir = new Yakuza("Yaku Le Noir", "whisky", 30, "Warsong", 4); // Réputation à 4
+        roro = new Ronin("Roro", 60); 
+
+        roro.provoquer(yakuLeNoir);
 	}
 
 }
